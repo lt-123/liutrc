b3b0f69f725afa79549dca4b347cb5138f0f0ec6
liutong <liut@koal.com>
2023-02-27 11:25:48 +0800

    fix ll alias

--- common/alias.sh
+++ common/alias.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
-alias ll='ls -lh'       # long format and human-readable sizes
-alias l='ll -A'         # long format, all files
-# shellcheck disable=SC2139
-alias lm="l | ${PAGER}" # long format, all files, use pager
-alias lr='ll -R'        # long format, recursive
-alias lk='ll -Sr'       # long format, largest file size last
-alias lt='ll -tr'       # long format, newest modification time last
-alias lc='lt -c'        # long format, newest status change (ctime) last
+# bash only
+if [ "$0" == 'bash' ]; then
+  alias ll='ls -lh' # long format and human-readable sizes
+  alias l='ll -A'   # long format, all files
+  # shellcheck disable=SC2139
+  alias lm="l | ${PAGER}" # long format, all files, use pager
+  alias lr='ll -R'        # long format, recursive
+  alias lk='ll -Sr'       # long format, largest file size last
+  alias lt='ll -tr'       # long format, newest modification time last
+  alias lc='lt -c'        # long format, newest status change (ctime) last
+fi
 
 alias ..='cd ..'
 alias ...='cd ../..'
