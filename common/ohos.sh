64d11ff4f9ce4c1fe282c30cdc9e12180647c78e
liut <lt-123@qq.com>
2025-04-29 14:32:23 +0800

    OHOS 添加 webview 连接 chrome 调试脚本

--- common/ohos.sh
+++ common/ohos.sh
@@ -13,3 +13,65 @@ function ohpm_install() {
     ohpm install --all  # 安装所有依赖
 }
 
+# 连接 chrome 调试
+# 来自 https://developer.huawei.com/consumer/cn/doc/harmonyos-guides/web-debugging-with-devtools#linux%E6%88%96mac%E5%B9%B3%E5%8F%B0
+function ohos_conn_web() {
+    # Get current fport rule list
+    CURRENT_FPORT_LIST=$(hdc fport ls)
+
+    # Delete the existing fport rule one by one
+    while IFS= read -r line; do
+        # Extract the taskline
+        IFS=' ' read -ra parts <<< "$line"
+        taskline="${parts[1]} ${parts[2]}"
+
+        # Delete the corresponding fport rule
+        echo "Removing forward rule for $taskline"
+        hdc fport rm $taskline
+        result=$?
+
+        if [ $result -eq 0 ]; then
+            echo "Remove forward rule success, taskline:$taskline"
+        else
+            echo "Failed to remove forward rule, taskline:$taskline"
+        fi
+
+    done <<< "$CURRENT_FPORT_LIST"
+
+    # Initial port number
+    INITIAL_PORT=9222
+
+    # Get the current port number, use initial port number if not set previously
+    CURRENT_PORT=${PORT:-$INITIAL_PORT}
+
+    # Get the list of all PIDs that match the condition
+    PID_LIST=$(hdc shell cat /proc/net/unix | grep webview_devtools_remote_ | awk -F '_' '{print $NF}')
+
+    if [ -z "$PID_LIST" ]; then
+        echo "Failed to retrieve PID from the device"
+        exit 1
+    fi
+
+    # Increment the port number
+    PORT=$CURRENT_PORT
+
+    # Forward ports for each application one by one
+    for PID in $PID_LIST; do
+        # Increment the port number
+        PORT=$((PORT + 1))
+
+        # Execute the hdc fport command
+        hdc fport tcp:$PORT localabstract:webview_devtools_remote_$PID
+
+        # Check if the command executed successfully
+        if [ $? -ne 0 ]; then
+            echo "Failed to execute hdc fport command"
+            exit 1
+        fi
+    done
+
+    # List all forwarded ports
+    hdc fport ls
+
+}
+
