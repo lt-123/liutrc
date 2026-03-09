816850dbe60aa8140b99232368fdb66359ef8b78
liut <lt-123@qq.com>
2026-03-09 17:33:54 +0800

    comment out aliases that may affect AI functionality in modern_cmd.sh

--- common/modern_cmd.sh
+++ common/modern_cmd.sh
@@ -3,13 +3,13 @@
 #eval "$(starship init zsh)"
 #eval "$(zoxide init zsh)"
 
-alias cat=bat
+#alias cat=bat 可能会影响 AI
 alias top=btm
 #alias htop=btm
 alias du=dust
 alias ls=lsd
 alias tree='lsd --tree'
 alias find=fd
-alias grep=rg
+#alias grep=rg 可能会影响 AI
 #alias man=tldr
 
